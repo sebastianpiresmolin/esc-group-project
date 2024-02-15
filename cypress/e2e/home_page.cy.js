b95c14d7b7df0c317287f60775928c7b5c83a6a7
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-02-15 15:11:59 +0100

    adding VG requirements to test

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,5 +1,26 @@
 describe("The Home Page", () => {
   it("The home page loaded successfully!", () => {
     cy.visit("https://sebastianpiresmolin.github.io/esc-group-project/");
+    cy.wait(200);
+    cy.contains("li", "Play on-site").click();
+    cy.wait(200);
+    cy.contains("button", "Filter challenges").click();
+    cy.wait(200);
+    cy.contains("label", "CTF").click();
+    cy.wait(200);
+    cy.get("#ctf").should("exist");
+    cy.wait(200);
+    cy.contains("li", "Contact us").click();
+    cy.wait(200);
+    cy.contains("li", "The story").click();
+    cy.wait(200);
+    cy.contains("li", "Play online").click();
+    cy.wait(200);
+    cy.contains("button", "Book this room").click();
+    cy.wait(200);
+    cy.contains("button", "Search available times").click();
+    cy.wait(200);
+    cy.contains("button", "Submit booking").click();
+    cy.wait(200);
   });
 });
