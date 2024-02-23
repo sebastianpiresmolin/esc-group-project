e0083ae329cbf45d57c64edb292c274397934998
AntonisTerzo <113201765+AntonisTerzo@users.noreply.github.com>
2024-02-23 18:34:56 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://sebastianpiresmolin.github.io/esc-group-project',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
