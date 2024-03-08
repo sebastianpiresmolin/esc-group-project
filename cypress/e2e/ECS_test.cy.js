c821c7e5e82c1729155abdf58596f480a0e6796e
rl-86 <rl86@live.se>
2024-03-08 21:29:11 +0100

    localhost test

--- cypress/e2e/ECS_test.cy.js
+++ cypress/e2e/ECS_test.cy.js
@@ -1,6 +1,6 @@
 describe('Check if site is live', () => {
   it('should have correct H1 heading', () => {
-    cy.visit('https://sebastianpiresmolin.github.io/esc-group-project/');
+    cy.visit('localhost:5501');
     cy.get('h1').contains('Hacker Escape Rooms');
   });
 });
