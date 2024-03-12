e52c433df17d0569aeb2802213bda203fcd6d2ea
rl-86 <rl86@live.se>
2024-03-12 09:06:41 +0100

    double quotes

--- cypress/e2e/ECS_test.cy.js
+++ cypress/e2e/ECS_test.cy.js
@@ -1,49 +1,49 @@
-describe('Check to see if website is working', () => {
+describe("Check to see if website is working", () => {
   //Check Heading
-  it('should have heading', () => {
-    cy.visit('https://sebastianpiresmolin.github.io/esc-group-project/');
+  it("should have heading", () => {
+    cy.visit("https://sebastianpiresmolin.github.io/esc-group-project/");
 
-    cy.get('h1').contains('Hacker Escape Rooms');
+    cy.get("h1").contains("Hacker Escape Rooms");
   });
 
   //Check Navbar
-  it('should have Navbar', () => {
-    cy.visit('/');
-    cy.get('nav.navbar').find('li').should('have.length', 4);
+  it("should have Navbar", () => {
+    cy.visit("/");
+    cy.get("nav.navbar").find("li").should("have.length", 4);
   });
 
   //Check Top3 Challenges ( This Test should fail)
-  it('should have Top3 Challenges', () => {
-    cy.visit('/');
-    cy.get('.challenge.animation_finished_loading').should('have.length', 3);
+  it("should have Top3 Challenges", () => {
+    cy.visit("/");
+    cy.get(".challenge.animation_finished_loading").should("have.length", 3);
   });
 
   //Check Footer
-  it('should have Footer', () => {
-    cy.visit('/');
-    cy.get('.footer');
+  it("should have Footer", () => {
+    cy.visit("/");
+    cy.get(".footer");
   });
 
   //Check Challenges Page
-  it('should have Challenges Page', () => {
-    cy.visit('/challenges.html');
-    cy.get('h1').contains('challenges');
+  it("should have Challenges Page", () => {
+    cy.visit("/challenges.html");
+    cy.get("h1").contains("challenges");
   });
 
   //Check Filters
-  it('should have Filters', () => {
-    cy.visit('/challenges.html');
-    cy.get('.filterButton').click();
-    cy.get('.filterWindow').should('exist');
+  it("should have Filters", () => {
+    cy.visit("/challenges.html");
+    cy.get(".filterButton").click();
+    cy.get(".filterWindow").should("exist");
 
-    cy.get('#web')
-      .should('have.text', 'Web')
+    cy.get("#web")
+      .should("have.text", "Web")
       .click()
-      .should('have.class', 'active')
+      .should("have.class", "active")
       .click()
-      .should('not.have.class', 'active')
-      .get('.challenges__container')
-      .find('h2')
-      .should('have.length', 30);
+      .should("not.have.class", "active")
+      .get(".challenges__container")
+      .find("h2")
+      .should("have.length", 30);
   });
 });
